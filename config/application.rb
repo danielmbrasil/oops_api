eb28b6087fe1247a34a64ccbe538587466ec9991
danielmbrasil <danielmbrasil@protonmail.com>
2021-06-08 09:58:28 -0300

    set default locale as Portuguese add devise messages Portuguese translation

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,12 @@ module OopsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # I18n config
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    I18n.default_locale = :pt
+
+    I18n.available_locales = :pt
   end
 end
